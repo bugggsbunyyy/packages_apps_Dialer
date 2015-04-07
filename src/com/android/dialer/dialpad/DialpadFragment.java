9edea65434f026acdcf2633351c632a69d68851b
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2015-04-07 18:54:50 +0200

    [2/3] Dialer: Smart Dialer
    Put on your ear, and this will automatically call current number in message or dialer
    
    Change-Id: I424175181c3c71ae83d9605ce017506340c83ab2
    Signed-off-by: linuxxxxx <joey@cyanogenmoditalia.it>
    
    Conflicts:
    	src/com/android/dialer/dialpad/DialpadFragment.java

--- src/com/android/dialer/dialpad/DialpadFragment.java
+++ src/com/android/dialer/dialpad/DialpadFragment.java
@@ -26,6 +26,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -34,6 +35,7 @@ import android.media.AudioManager;
 import android.media.ToneGenerator;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.provider.Contacts.People;
 import android.provider.Contacts.Phones;
 import android.provider.Contacts.PhonesColumns;
@@ -73,6 +75,7 @@ import com.android.contacts.common.CallUtil;
 import com.android.contacts.common.ContactsUtils;
 import com.android.contacts.common.GeoUtil;
 import com.android.contacts.common.MoreContactUtils;
+import com.android.contacts.common.util.PickupGestureDetector;
 import com.android.contacts.common.util.PhoneNumberFormatter;
 import com.android.contacts.common.util.StopWatch;
 import com.android.contacts.common.widget.FloatingActionButtonController;
@@ -82,6 +85,7 @@ import com.android.dialer.R;
 import com.android.dialer.SpecialCharSequenceMgr;
 import com.android.dialer.SpeedDialListActivity;
 import com.android.dialer.SpeedDialUtils;
+import com.android.dialer.settings.GeneralSettingsFragment;
 import com.android.dialer.util.DialerUtils;
 import com.android.dialerbind.analytics.AnalyticsFragment;
 import com.android.internal.telephony.PhoneConstants;
@@ -105,6 +109,7 @@ public class DialpadFragment extends AnalyticsFragment
         View.OnLongClickListener, View.OnKeyListener,
         AdapterView.OnItemClickListener, TextWatcher,
         PopupMenu.OnMenuItemClickListener,
+        PickupGestureDetector.PickupListener,
         DialpadKeyButton.OnPressedListener {
     private static final String TAG = DialpadFragment.class.getSimpleName();
 
@@ -197,6 +202,8 @@ public class DialpadFragment extends AnalyticsFragment
     private ToneGenerator mToneGenerator;
     private final Object mToneGeneratorLock = new Object();
     private View mSpacer;
+
+    private PickupGestureDetector mPickupDetector;
 
     private FloatingActionButtonController mFloatingActionButtonController;
 
@@ -302,6 +309,11 @@ public class DialpadFragment extends AnalyticsFragment
         return (TelephonyManager) getActivity().getSystemService(Context.TELEPHONY_SERVICE);
     }
 
+    private boolean isSmartCallEnabled() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        return prefs.getBoolean(GeneralSettingsFragment.BUTTON_SMART_DIALER_KEY, false);
+    }
+
     private TelecomManager getTelecomManager() {
         return (TelecomManager) getActivity().getSystemService(Context.TELECOM_SERVICE);
     }
@@ -368,6 +380,8 @@ public class DialpadFragment extends AnalyticsFragment
         }
 
         mDialpadSlideInDuration = getResources().getInteger(R.integer.dialpad_slide_in_duration);
+
+        mPickupDetector = new PickupGestureDetector(getActivity(), this);
     }
 
     @Override
@@ -715,6 +729,10 @@ public class DialpadFragment extends AnalyticsFragment
 
         stopWatch.stopAndLog(TAG, 50);
 
+        if (!isPhoneInUse() && isSmartCallEnabled()) {
+            mPickupDetector.enable();
+        }
+
         mSmsPackageComponentName = DialerUtils.getSmsComponent(activity);
 
         // Populate the overflow menu in onResume instead of onCreate, so that if the SMS activity
@@ -749,6 +767,8 @@ public class DialpadFragment extends AnalyticsFragment
         mLastNumberDialed = EMPTY_NUMBER;  // Since we are going to query again, free stale number.
 
         SpecialCharSequenceMgr.cleanup();
+
+        mPickupDetector.disable();
     }
 
     @Override
@@ -758,6 +778,21 @@ public class DialpadFragment extends AnalyticsFragment
         if (mClearDigitsOnStop) {
             mClearDigitsOnStop = false;
             clearDialpad();
+        }
+    }
+
+    @Override
+    public void onPickup() {
+        if (!isDigitsEmpty()) {
+            mPickupDetector.disable();
+
+            final String number = mDigits.getText().toString();
+            final DialtactsActivity activity = getActivity() instanceof DialtactsActivity
+                    ? (DialtactsActivity) getActivity() : null;
+            final Intent intent = CallUtil.getCallIntent(number,
+                    activity != null ? activity.getCallOrigin() : null);
+            startActivity(intent);
+            hideAndClearDialpad(false);
         }
     }
 
